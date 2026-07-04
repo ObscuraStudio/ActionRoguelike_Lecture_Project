a2fda1121fc3bca58ca07779d27b8a991f278c9f
ObscuraStudio <maximilian.tofelde@gmail.com>
2026-07-04 19:02:34 +0200

    Added Projectiles

---
+++ Source/ActionRoguelike/Projectiles/RogueProjectileMagic.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RogueProjectileMagic.h"
+
+#include "NiagaraComponent.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ARogueProjectileMagic::ARogueProjectileMagic()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	RootComponent = SphereComponent;
+	SphereComponent->SetSphereRadius(16.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Projectile"));
+	
+	LoopedNiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LoopedNiagaraComp"));
+	LoopedNiagaraComponent->SetupAttachment(SphereComponent);
+	
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMoveComp"));
+	ProjectileMovementComponent->InitialSpeed = 500.0f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+	
+	
+}
